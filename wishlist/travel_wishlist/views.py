5fdf204fc2f02df4ef2bf3352cbbc9a2fa603600
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-10 19:21:09 -0500

    Create views for place_list function

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
 def place_list(request):
-    return render(request, 'travel_wishlist/wishlist.html')
+    places = Place.objects.filter(visited=False)
+    return render(request, 'travel_wishlist/wishlist.html', { 'places': places })
