1e89bffeba0fad135f313a19941e4eb75592dc8c
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-13 11:41:45 -0500

    Adding comments

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 
-# Create your views here.
+# View for place list
 def place_list(request):
 
     # if POST request where the user click the add button save a new place
@@ -14,19 +14,25 @@ def place_list(request):
             place.save() # Save to the database
             return redirect('place_list') # Redirect to GET view with name place_list
 
+    # if not POST, or the form is not valid, render the page
+    # with the form to add a new place, and list of places
     places = Place.objects.filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', { 'places': places, 'new_place_form': new_place_form })
 
+# View for visited places
 def places_visited(request):
 
     visited = Place.objects.filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', { 'visited': visited})
 
+# View for if the place was visited by the user
 def place_was_visited(request):
     
+    # if POST where the user click the visited button save as a visited place
+    # to the database and redirect it to GET view of place_list
     if request.method == 'POST':
-        pk = request.POST.get('pk')
+        pk = request.POST.get('pk') 
         place = get_object_or_404(Place, pk=pk)
         place.visited = True
         place.save()
