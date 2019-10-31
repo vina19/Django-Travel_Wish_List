7384e68ac5fbc93276e98e23432805739d478044
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-31 10:02:46 -0500

    Adding place_detail

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 @login_required
 def place_list(request):
@@ -29,8 +30,12 @@ def place_was_visited(request):
     if request.method == 'POST':
         pk = request.POST.get('pk')
         place = get_object_or_404(Place,pk=pk)
-        place.visited = True
-        place.save()
+        print(place.user, request.user)
+        if place.user == request.user:
+            place.visited = True
+            place.save()
+        else:
+            return HttpResponseForbidden()
     
     return redirect('place_list')
 
