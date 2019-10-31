196c0aaeb7d3a47096ad918a0f189d063355152b
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2019-10-31 10:17:13 -0500

    Adding image and js

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.place_list, name='place_list'),
     path('visited', views.places_visited, name='places_visited'),
     path('was_visited', views.place_was_visited, name='place_was_visited'),
-    path('place/<int:place_pk>', views.place_details, name='place_details')
+    path('place/<int:place_pk>', views.place_details, name='place_details'),
+    path('delete', views.delete_place, name='delete_place')
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -4,6 +4,7 @@ from .forms import NewPlaceForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 
+# Show the view of the place list
 @login_required
 def place_list(request):
     if request.method == 'POST':
@@ -20,13 +21,18 @@ def place_list(request):
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', { 'places': places, 'new_place_form': new_place_form })
 
+# View for visited places
 @login_required
 def places_visited(request):
     visited = Place.objects.filter(user=request.user).filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', { 'visited': visited })
 
+# View for if the place was visited by the user
 @login_required
 def place_was_visited(request):
+    
+    # if POST where the user click the visited button save as a visited place
+    # to the database and redirect it to GET view of place_list
     if request.method == 'POST':
         pk = request.POST.get('pk')
         place = get_object_or_404(Place,pk=pk)
@@ -39,44 +45,19 @@ def place_was_visited(request):
     
     return redirect('place_list')
 
+# Show the place detail view
 @login_required
 def place_details(request, place_pk):
     place = get_object_or_404(Place, pk=place_pk)
     return render(request, 'travel_wishlist/place_detail.html', {'place': place} )
 
-# View for place list
-def place_list(request):
-
-    # if POST request where the user click the add button save a new place
-    # to the database and redirect it to the same page 
-    if request.method == 'POST':
-        form = NewPlaceForm(request.POST) 
-        place = form.save() # Create a new Place from the form
-        if form.is_valid(): # Check if the required fields are present
-            place.save() # Save to the database
-            return redirect('place_list') # Redirect to GET view with name place_list
-
-    # if not POST, or the form is not valid, render the page
-    # with the form to add a new place, and list of places
-    places = Place.objects.filter(visited=False).order_by('name')
-    new_place_form = NewPlaceForm()
-    return render(request, 'travel_wishlist/wishlist.html', { 'places': places, 'new_place_form': new_place_form })
-
-# View for visited places
-def places_visited(request):
-
-    visited = Place.objects.filter(visited=True)
-    return render(request, 'travel_wishlist/visited.html', { 'visited': visited})
-
-# View for if the place was visited by the user
-def place_was_visited(request):
-    
-    # if POST where the user click the visited button save as a visited place
-    # to the database and redirect it to GET view of place_list
-    if request.method == 'POST':
-        pk = request.POST.get('pk') 
-        place = get_object_or_404(Place, pk=pk)
-        place.visited = True
-        place.save()
-    
-    return redirect('place_list')
+# Create a delete option view
+@login_required
+def delete_place(request):
+    pk = request.POST.get('pk')
+    place = get_object_or_404(Place, pk=pk)
+    if place.user == request.user:
+        place.delete()
+        return redirect('place_list')
+    else:
+        return HttpResponseForbidden()
